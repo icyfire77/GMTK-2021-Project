660877b9a78e8bd0a0e7576bd0e35e2446650546
icyfire77 <ivanfeng1@hotmail.com>
2021-06-11 15:18:59 -0700

    magnet

--- main.lua
+++ main.lua
@@ -1,13 +1,12 @@
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
 
 function love.load()
   Object = require "classic"
   love.graphics.setDefaultFilter('nearest', 'nearest')
-end
-
-function love.draw()
+  currentScreen = "menu"
 end
 
 function love.update(dt)
+end
+
+function love.draw()
 end
--- player.lua
+++ player.lua
@@ -1,2 +1,29 @@
 -- based
-Magnet = Object.extend(Object)
+Magnet = Object:extend()
+
+function Magnet:new(dir, x, y, width, height)
+  self.x = x
+  self.y = y
+  self.width = width
+  self.height = height
+  self.vel = 0
+  self.dir = dir
+end
+
+function Magnet:update(dt)
+  if self.dir == "left" then
+    self.x = self.x - self.vel*dt
+  else
+    self.x = self.x + self.vel*dt
+end
+
+function Magnet:accelerate(speed)
+  if self.dir == "left" then
+    self.vel = self.vel - speed
+  else
+    self.vel = self.vel + speed
+  end
+end
+
+function Magnet:draw()
+end
