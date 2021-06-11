01c640f3b963b442d9dfeb6a4985ceb147042adf
icyfire77 <ivanfeng1@hotmail.com>
2021-06-11 14:08:02 -0700

    stubs

--- main.lua
+++ main.lua
@@ -1,1 +1,12 @@
--- breuh --
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+end
+
+function love.draw()
+end
+
+function love.update(dt)
+end
