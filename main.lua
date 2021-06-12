6b4b0e39d2f8be9f843666610c340e09ef0f166c
Shawn <shawnlu4@gmail.com>
2021-06-12 16:19:22 -0700

    added level select

--- main.lua
+++ main.lua
@@ -4,11 +4,15 @@ function newButton(text, fn)
     text = text,
     fn = fn,
     now = false,
-    last = false
+    last = false,
+    x = 0,
+    y = 0
   }
 end
 
-local buttons = {}
+local menuButtons = {}
+local levelButtons = {}
+
 local creditsVel = 100
 
 function love.load()
@@ -26,7 +30,11 @@ function love.load()
   magnetAccel = 10
   releaseFrames = 0
   releaseCounter = 0
-  strength = 5
+  strength = 10
+
+  -- for level select
+  clickedx = 0
+  diffx = 0
 
   playerL = Magnet("left", 0, 650, 50, 20)
   playerR = Magnet("right", love.graphics.getWidth() -50, 650, 50, 20)
@@ -36,27 +44,30 @@ function love.load()
 
   -- menu logic (WIP)
   currentScreen = "menu"
-  table.insert(buttons, newButton("Start",
+  table.insert(menuButtons, newButton("Start",
     function()
       -- Likely first change this to some tutorial page later
-      currentScreen = "levelOne"
-      print(currentScreen)
-      sound = love.audio.newSource(
-        "Different_Heaven_-_Nekozilla.mp3", "stream")
-      love.audio.play(sound)
-      sound:setVolume(0.3)
-      levels:setLevel(1)
+      
+      -- currentScreen = "levelOne"
+      -- print(currentScreen)
+      -- sound = love.audio.newSource(
+      --   "Different_Heaven_-_Nekozilla.mp3", "stream")
+      -- love.audio.play(sound)
+      -- sound:setVolume(0.3)
+      -- levels:setLevel(1)
+
+      currentScreen = "SelectLevel"
     end
   ))
 
-  table.insert(buttons, newButton("Credits",
+  table.insert(menuButtons, newButton("Credits",
     function()
       currentScreen = "credits"
       print("none for now, WIP")
     end
   ))
 
-  table.insert(buttons, newButton("Exit",
+  table.insert(menuButtons, newButton("Exit",
     function()
       love.event.quit(0)
     end
@@ -64,7 +75,7 @@ function love.load()
 end
 
 function love.update(dt)
-  if currentScreen == "levelOne" then
+  if currentScreen:find("level", 1, true) == 1 then
       levels:generate(sound:tell())
       levels:update(dt)
 
@@ -99,8 +110,13 @@ function love.draw()
   local buttonHeight = 70
   local margin = 30
 
-  local totalHeight = (buttonHeight + margin)*#buttons
+  local totalHeight = (buttonHeight + margin)*#menuButtons
   local cursorY = 0
+
+  -- for level select
+  local gap = 500
+  local levelButtonRadius = 200
+
 
   if currentScreen == "levelOne" then
     playerL:draw()
@@ -109,18 +125,25 @@ function love.draw()
     love.graphics.print(levels.score, menuFont, 0, 0)
   end
 
+  if currentScreen == "levelTwo" then
+    playerL:draw()
+    playerR:draw()
+    levels:draw()
+    love.graphics.print(levels.score, menuFont, 0, 0)
+  end
+
   if currentScreen == "menu" then
-    for i, button in ipairs(buttons) do
+    for i, button in ipairs(menuButtons) do
       button.last = button.now
 
-      local buttonX = windowWidth/2 - buttonWidth/2
-      local buttonY = windowHeight/2 - totalHeight/2 + cursorY
+      button.x = windowWidth/2 - buttonWidth/2
+      button.y = windowHeight/2 - totalHeight/2 + cursorY
 
       local colour = {0.5, 0.8, 0.7, 1}
       local mx, my = love.mouse.getPosition()
 
-      local hot = mx > buttonX and mx < buttonX + buttonWidth and
-        my > buttonY and my < buttonY + buttonHeight
+      local hot = mx > button.x and mx < button.x + buttonWidth and
+        my > button.y and my < button.y + buttonHeight
 
       if hot then
         colour = {0.7, 0.9, 0.8, 1}
@@ -134,7 +157,7 @@ function love.draw()
       -- setColor uses RGB and alpha
       love.graphics.setColor(unpack(colour))
       love.graphics.rectangle("fill",
-      buttonX, buttonY,
+      button.x, button.y,
       buttonWidth, buttonHeight)
 
       local textWidth = menuFont:getWidth(button.text)
@@ -145,17 +168,111 @@ function love.draw()
         button.text,
         menuFont,
         windowWidth/2 - textWidth/2,
-        buttonY + textHeight/5)
+        button.y + textHeight/5)
 
         cursorY = cursorY + (buttonHeight + margin)
     end
   end
+
   if currentScreen == "credits" then
     local creditsText = "Haha Magnet Game" -- TODO: WRITE ACTUAL CREDITS
     love.graphics.setColor(1, 1, 1, 1)
     love.graphics.print(creditsText,
       menuFont, -- TODO: CHANGE MENU FONT TO SOME OTHER FONT
       windowWidth/2 - menuFont:getWidth(creditsText)/2, creditsY)
+  end
+
+  if currentScreen == "SelectLevel" then
+    if #levelButtons <= 0 then
+      table.insert(levelButtons, newButton("Level 1",
+        function()
+          currentScreen = "levelOne"
+          sound = love.audio.newSource(
+            "Different_Heaven_-_Nekozilla.mp3", "stream")
+          love.audio.play(sound)
+          sound:setVolume(0.3)
+          levels:setLevel(1)
+        end
+      ))
+      table.insert(levelButtons, newButton("Level 2",
+        function()
+          currentScreen = "levelTwo"
+          sound = love.audio.newSource(
+            "Different_Heaven_-_Nekozilla.mp3", "stream")
+          love.audio.play(sound)
+          sound:setVolume(0.3)
+          levels:setLevel(2)
+        end
+      ))
+      table.insert(levelButtons, newButton("Coming Soon",
+        function()
+
+        end
+      ))
+      for i, button in ipairs(levelButtons) do
+        button.now = true
+        button.x = windowWidth/2 + gap * (i-1)
+        button.y = windowHeight/2 
+      end
+    end
+
+    for i, button in ipairs(levelButtons) do
+      button.last = button.now
+
+      local colour = {0.5, 0.8, 0.7, 1}
+      local mx, my = love.mouse.getPosition()
+      local actualdiffx = 0
+
+      local hot = (button.x - mx) * (button.x - mx) + (button.y - my) * (button.y - my) <= levelButtonRadius * levelButtonRadius
+
+      if hot then
+        colour = {0.7, 0.9, 0.8, 1}
+      end
+
+      button.now = love.mouse.isDown(1)
+
+      if button.now and not button.last then
+        clickedx = mx
+        if hot then
+          button.fn()
+        end
+      end
+      if not button.now and button.last then
+        if clickedx > 0 then
+          button.x = button.x + diffx
+        end  
+        actualdiffx = 0
+        if i == #levelButtons then
+          diffx = 0
+        end
+      end
+      if button.now then
+        diffx = mx - clickedx
+        -- just to catch in the bery beginning button.now == true but diffx shouldnt be set
+        if clickedx <= 0 then
+          actualdiffx = 0
+        else
+          actualdiffx = diffx
+        end
+      end
+      print(button.x)
+      -- setColor uses RGB and alpha
+      love.graphics.setColor(unpack(colour))
+      love.graphics.circle("fill",
+      button.x + actualdiffx, button.y,
+      levelButtonRadius)
+
+      local textWidth = menuFont:getWidth(button.text)
+      local textHeight = menuFont:getHeight(button.text)
+
+      love.graphics.setColor(0, 0, 0, 1)
+      love.graphics.print(
+        button.text,
+        menuFont,
+        button.x - textWidth/2 + actualdiffx,
+        button.y - textHeight/5)
+
+    end
   end
 end
 
