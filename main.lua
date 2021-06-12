a7bdc2cad90cd0d59c94f7fcc3cdba887535f17b
icyfire77 <ivanfeng1@hotmail.com>
2021-06-11 19:26:24 -0700

    broken menu

--- main.lua
+++ main.lua
@@ -2,7 +2,9 @@
 function newButton(text, fn)
   return {
     text = text,
-    fn = fn
+    fn = fn,
+    now = false,
+    last = false
   }
 end
 
@@ -12,12 +14,31 @@ function love.load()
   Object = require "classic"
   require "player"
   love.graphics.setDefaultFilter('nearest', 'nearest')
+  menuFont = love.graphics.newFont("Mick Caster.ttf", 40)
+
+  -- useful global/local variables?
+  windowWidth = love.graphics.getWidth()
+  windowHeight = love.graphics.getHeight()
+  magnetAccel = 10
+  strength = 15
+
+  playerL = Magnet("left", 0, 650, 50, 20)
+  playerR = Magnet("right", windowWidth - 50, 650, 50, 20)
 
 -- menu logic (WIP)
   currentScreen = "menu"
   table.insert(buttons, newButton("Start",
     function()
+      -- Likely first change this to some tutorial page later
+      currentScreen = "levelOne"
       print("Starting Game")
+    end
+  ))
+
+  table.insert(buttons, newButton("Credits",
+    function()
+      currentScreen = "credits"
+      print("none for now, WIP")
     end
   ))
 
@@ -27,14 +48,6 @@ function love.load()
     end
   ))
 
--- useful global/local variables?
-  windowWidth = love.graphics.getWidth()
-  windowHeight = love.graphics.getHeight()
-  magnetAccel = 10
-  strength = 15
-
-  playerL = Magnet("left", 0, 650, 50, 20)
-  playerR = Magnet("right", love.graphics.getWidth()-50, 650, 50, 20)
 end
 
 function love.update(dt)
@@ -53,7 +66,57 @@ function love.update(dt)
 end
 
 function love.draw()
-  --for i, button in ipairs(buttons) do
-  playerL:draw()
-  playerR:draw()
+  local buttonWidth = windowWidth/3
+  local buttonHeight = 70
+  local margin = 30
+
+  local totalHeight = (buttonHeight + margin)*#buttons
+  local cursorY = 0
+
+  if currentScreen == "levelOne" then
+    playerL:draw()
+    playerR:draw()
+  end
+
+  if currentScreen == "menu" then
+    for i, button in ipairs(buttons) do
+      button.last = button.now
+
+      local buttonX = windowWidth/2 - buttonWidth/2
+      local buttonY = windowHeight/2 - totalHeight/2 + cursorY
+
+      local colour = {0.5, 0.8, 0.7, 1}
+      local mx, my = love.mouse.getPosition()
+
+      local hot = mx > buttonX and mx < buttonX + buttonWidth and
+        my > buttonY and my < buttonY + buttonHeight
+
+      if hot then
+        colour = {0.7, 0.9, 0.8, 1}
+      end
+
+      button.now = love.mouse.isDown(1)
+      if button.now and not button.last and hot then
+        button.fn()
+      end
+
+      -- setColor uses RGB and alpha
+      love.graphics.setColor(unpack(colour))
+      love.graphics.rectangle("fill",
+      buttonX, buttonY,
+      buttonWidth, buttonHeight)
+
+      local textWidth = menuFont:getWidth(button.text)
+      local textHeight = menuFont:getHeight(button.text)
+
+      love.graphics.setColor(0, 0, 0, 1)
+      love.graphics.print(
+        button.text,
+        menuFont,
+        windowWidth/2 - textWidth/2,
+        buttonY + textHeight/5)
+
+        cursorY = cursorY + (buttonHeight + margin)
+    end
+  end
 end
