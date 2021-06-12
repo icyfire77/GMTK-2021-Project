5b9e81891a9d3aaac0dce9079874b48f4ff46f71
Nick Nadeau <j.nicholas.nadeau@outlook.com>
2021-06-11 19:26:43 -0700

    enemy

---
+++ enemy.lua
@@ -0,0 +1,15 @@
+-- actually based as hell
+Enemy = Object:extend()
+
+function Enemy:new(x, y, w, h)
+  self.x, self.y = x, y
+  self.width, self.height = w, h
+end
+
+function Enemy:udpate(dt)
+  self.y = self.y - dt
+end
+
+function Enemy:draw()
+  love.graphics.circle("fill", self.x, self.y, self.width, self.height)
+end
